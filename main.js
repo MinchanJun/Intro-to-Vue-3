f8d840aaf4c472247c8604db25aacfc45b55d5c5
MinchanJun <minchan@Minchans-MacBook-Pro.local>
2020-11-19 00:25:32 -0500

    conditional rendering

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inStock: false,
+            inStockk: false,
+            inventory: 999,
+            onSale: true
         }
     }
 })
